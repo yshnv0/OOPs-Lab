e8768c6161639e358c2118997cbeb01f323014f1
Arun S Nair <93431805+arunsnair135@users.noreply.github.com>
2022-03-21 22:45:42 +0530

    Add files via upload
    pgrm 1

--- co1/product.java
+++ co1/product.java
@@ -1,49 +1,31 @@
 import java.util.Scanner;
 
-public class product {
+class product{
 	int pcode,price;
-	String pname=new String();
-	product(int a,String b,int c){
-		pcode=a;
-		pname=b;
-		price=c;
-		
-	}
-	
-	void display(){
-			System.out.println("CODE: "+pcode);
-			System.out.println("Name: "+pname);
-			System.out.println("Price: "+price);
-	}
-	
-	public static void main(String[] args) {
+	String pname;
+	void input(){
 		Scanner s=new Scanner(System.in);
 		System.out.println("Enter the product code ");
-		int a1=s.nextInt();
+		pcode=s.nextInt();
 		s.nextLine();
 		System.out.println("Enter the product name ");
-		String b1=s.nextLine();
+		pname=s.nextLine();
 		System.out.println("Enter the product price ");
-		int c1=s.nextInt();
-		product obj1=new product(a1,b1,c1);
-		
-		System.out.println("Enter the product code ");
-		int a2=s.nextInt();
-		s.nextLine();
-		System.out.println("Enter the product name ");
-		String b2=s.nextLine();
-		System.out.println("Enter the product price ");
-		int c2=s.nextInt();
-		product obj2=new product(a2,b2,c2);
-		
-		System.out.println("Enter the product code ");
-		int a3=s.nextInt();
-		s.nextLine();
-		System.out.println("Enter the product name ");
-		String b3=s.nextLine();
-		System.out.println("Enter the product price ");
-		int c3=s.nextInt();
-		product obj3=new product(a3,b3,c3);
+		price=s.nextInt();
+	}
+	void display(){
+		System.out.println(pcode);
+		System.out.println(pname);
+		System.out.println(price);
+	}
+	
+	public static void main(String[] args){
+		product obj1=new product();
+		obj1.input();
+		product obj2=new product();
+		obj2.input();
+		product obj3=new product();
+		obj3.input();
 		System.out.println("First Object");
 		obj1.display();
 		System.out.println("Second Object");
@@ -52,17 +34,18 @@ public class product {
 		obj3.display();
 		
 		if(obj1.price > obj2.price && obj1.price > obj3.price){
-				System.out.println("First obj has highest price");
+			System.out.println("First Object has highest price");
 		}
 		
 		if(obj2.price > obj1.price && obj2.price > obj3.price){
-			System.out.println("Second obj has highest price");
-	}
+			System.out.println("Second Object has highest price");
+		}
 		
-		if(obj3.price > obj1.price && obj3.price > obj2.price){
-			System.out.println("Third obj has highest price");
-	}
+		if(obj3.price > obj2.price && obj3.price > obj1.price){
+			System.out.println("Third Object has highest price");
+		}
+		
 		
 	}
-
 }
+
