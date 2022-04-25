8c0e87cac153a1e45e4910091284bc942208d070
Arun S Nair <93431805+arunsnair135@users.noreply.github.com>
2022-04-25 11:52:21 +0530

    Add files via upload

---
+++ CO3/overload.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class overload {
+	void area(int a){
+		System.out.println("Area of Square= "+a*a);
+	}
+	void area(int l,int b){
+		System.out.println("Area of Rectangle= "+l*b);
+	}
+	void area(float r){
+		System.out.println("Area of Circle= "+2*3.14*r);
+	}
+	
+	public static void main(String[] args) {
+		int ch;
+		Scanner s =new Scanner(System.in);
+		overload fn=new overload();
+		System.out.println("1-Square\n2-Rectangle\n3-Circle\n0-Exit");
+		do{
+			System.out.println("Enter the choice");
+			ch=s.nextInt();
+			switch(ch){
+			case 1:System.out.println("Enter the side of the square");
+					int x=s.nextInt();
+					fn.area(x);
+					break;
+			case 2:System.out.println("Enter the length and breadth of the rectangle");
+					int y=s.nextInt();
+					int z=s.nextInt();
+					fn.area(y,z);
+					break;
+			case 3:System.out.println("Enter the radius of the circle ");
+					float r=s.nextFloat();
+					fn.area(r);
+					break;
+			}
+		}while(ch!=0);
+		s.close();
+	}
+
+}
