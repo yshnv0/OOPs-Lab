c9ab038808bf0a29a028f783afe739c251e90ba4
Arun S Nair <93431805+arunsnair135@users.noreply.github.com>
2022-04-25 12:03:00 +0530

    Update overload.java

--- CO3/overload.java
+++ CO3/overload.java
@@ -7,7 +7,7 @@ public class overload {
 		System.out.println("Area of Rectangle= "+l*b);
 	}
 	void area(float r){
-		System.out.println("Area of Circle= "+2*3.14*r);
+		System.out.println("Area of Circle= "+3.14*r*r);
 	}
 	
 	public static void main(String[] args) {
