a974a52887132fff48aaf153921bb0c09e73bc5e
Arun S Nair <93431805+arunsnair135@users.noreply.github.com>
2022-05-12 22:43:51 +0530

    Update 7 bill.java

--- CO3/7 bill.java
+++ CO3/7 bill.java
@@ -17,29 +17,43 @@ public class bill implements outline{
 		System.out.println("\nEnter the item unit price");
 		unit=s.nextInt();
 		s.nextLine();
-		
+		total=unit*quantity;
 		
 	}
 	public void prepareBill(){
-		System.out.println("PRODUCT ID\t NAME\t\tQUANTITY\tUNIT PRIZE\tTOTAL");
-		System.out.println(id+"\t\t "+name+"\t\t  "+quantity+"\t\t  "+unit+"\t\t"+(unit*quantity)+"\n");
+		
+		System.out.format("%10d %10s %10d %10d %10d",id,name,quantity,unit,total);
 	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int ch=1;
-		int n=5,i=0;
+		int n=5,i=0,net=0;
 		bill newbill[]=new bill[n];
 		while(ch==1 && i<n ){
-			System.out.println("Ttem 1");
+			System.out.println("Ttem "+(i+1));
 			newbill[i]=new bill();
 			i++;
 			System.out.println("Enter 1 to add more items");
 			ch=s.nextInt();
 		}
-		
+		System.out.printf("%10s %10s %10s %10s %10s","PRODUCT ID", "NAME", "QUANTITY", "UNIT PRIZE", "TOTAL");
+		System.out.println();
+		for(int z=0;z<55;z++){
+			System.out.print("-");
+		}
+		System.out.println();
 		for(int j=0;j<i;j++){
 			newbill[j].prepareBill();
+			System.out.println();
 		}
+		for(int z=0;z<55;z++){
+			System.out.print("-");
+		}
+		System.out.println();
+		for(int j=0;j<i;j++){
+			net+=newbill[j].total;
+		}
+		System.out.println("Net Amount :"+net);
 	}
 
 }
